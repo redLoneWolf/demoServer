e11f86c35009b39edb6afa25e2e84ff479bfa1ff
sudhar <sudhar00123@gmail.com>
2022-06-13 19:06:59 +0530

    added items patch

---
+++ src/main/java/com/example/demoserver/CustomServlet.java
@@ -0,0 +1,19 @@
+package com.example.demoserver;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public abstract class CustomServlet extends HttpServlet {
+    public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getMethod().equalsIgnoreCase("PATCH")){
+            doPatch(request, response);
+        } else {
+            super.service(request, response);
+        }
+    }
+
+    public abstract void doPatch(HttpServletRequest request, HttpServletResponse response) throws IOException;
+}
--- src/main/java/com/example/demoserver/ItemServlet.java
+++ src/main/java/com/example/demoserver/ItemServlet.java
@@ -3,6 +3,7 @@ package com.example.demoserver;
 import com.example.demoserver.data.Item;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -12,9 +13,12 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @WebServlet(name = "ItemServlet", value = "/items")
-public class ItemServlet extends HttpServlet {
+public class ItemServlet extends CustomServlet {
     Connection connection;
     Gson gson;
     @Override
@@ -22,8 +26,11 @@ public class ItemServlet extends HttpServlet {
         super.init();
         connection = Database.getConnection();
         gson = GsonHelper.getGson();
-
     }
+
+
+
+
 
     String getAll(){
         JsonArray jsonArray = new JsonArray();
@@ -116,5 +123,71 @@ public class ItemServlet extends HttpServlet {
             }else {
                 response.getOutputStream().print("Creation Failed");
             }
+    }
+
+//    boolean allowEdit(String key){
+//        return(key=="name"|| key=="desc"||key=="cost"||key=="sale");
+//    }
+
+    @Override
+    public void doPatch(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String body = Utils.getBody(request);
+        int objId = 0;
+        response.setStatus(200);
+        response.setHeader("Content-Type", "application/json");
+        if(request.getParameter("id")!=null){
+            objId = Integer.parseInt(request.getParameter("id"));
+        }else {
+            response.setStatus(400);
+            return;
+        }
+
+        JsonObject object =  gson.fromJson(body,JsonObject.class);
+
+//        response.getOutputStream().print(object.toString());
+//        "name","desc","cost","sale"
+
+//        if(!object.entrySet().contains("id")){
+//            response.setStatus(400);
+//
+//            return;
+//        }
+
+        StringBuilder query = new StringBuilder("update items set ");
+
+        for (Map.Entry<String, JsonElement> entry:object.entrySet()){
+            String key = entry.getKey();
+            if(key.equals("name")){
+
+                query.append("name='").append(entry.getValue().getAsString()).append("',");
+
+            }else if (key.equals("desc")){
+
+                query.append("description='").append(entry.getValue().getAsString()).append("',");
+            }
+            else if (key.equals("cost")){
+
+                query.append("costPrice=").append(entry.getValue().getAsString()).append(",");
+            }
+            else if (key.equals("sale")){
+
+                query.append("sellingPrice=").append(entry.getValue().getAsString()).append(" ");
+            }
+//            else if (key.equals("id")) {
+//                objId = entry.getValue().getAsInt();
+//            }
+        }
+        query.append(" where id=").append(objId).append(" ;");
+//        System.out.println(query);
+
+        try {
+            Statement st = connection.createStatement();
+            st.executeUpdate(query.toString());
+            response.getOutputStream().print("Success");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
